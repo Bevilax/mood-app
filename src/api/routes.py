5d544e6ff2224d74a2b34a4af13ef6a02ab31b9d
bryanhayslip <bryan.hayslip@gmail.com>
2022-06-08 00:12:26 +0000

    added protected route

--- src/api/routes.py
+++ src/api/routes.py
@@ -22,3 +22,10 @@ def create_token():
 
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
