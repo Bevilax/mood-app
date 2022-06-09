f2b952714b69983c5daae39d7fb007f8f0df0286
bryanhayslip <bryan.hayslip@gmail.com>
2022-06-09 21:03:20 +0000

    Added login function to Actions in flux.js

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,36 +1,20 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { useHistory } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Login = () => {
   const { store, actions } = useContext(Context);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const history = useHistory();
 
   const token = sessionStorage.getItem("token");
 
   const handleClick = () => {
-    const opts = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    };
-    fetch(process.env.BACKEND_URL + `/api/token/`, opts)
-      .then((resp) => {
-        if (resp.status === 200) return resp.json();
-        else alert("There was an error");
-      })
-      .then((data) => {
-        console.log(data);
-        sessionStorage.setItem("token", data.access_token);
-      })
-      .catch((error) => console.error("There's an error!", error));
+    actions.login(email, password).then(() => {
+      history.push("/");
+    });
   };
 
   return (
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,49 +1,85 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      token: null,
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      login: async (email, password) => {
+        const opts = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        };
+        try {
+          const resp = await fetch(
+            process.env.BACKEND_URL + `/api/token/`,
+            opts
+          );
 
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+          if (resp.status !== 200) {
+            alert("There was an error");
+            return false;
+          }
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+          const data = await resp.json();
+          console.log(data);
+          sessionStorage.setItem("token", data.access_token);
+          setStore({ token: data.access_token });
+          return true;
+        } catch (error) {
+          console.error("There has been an error logging in!");
+        }
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        //   .catch((error) => console.error("There's an error!", error));
+      },
+    },
+
+    exampleFunction: () => {
+      getActions().changeColor(0, "green");
+    },
+
+    getMessage: () => {
+      // fetching data from the backend
+      fetch(process.env.BACKEND_URL + "/api/hello")
+        .then((resp) => resp.json())
+        .then((data) => setStore({ message: data.message }))
+        .catch((error) =>
+          console.log("Error loading message from backend", error)
+        );
+    },
+    changeColor: (index, color) => {
+      //get the store
+      const store = getStore();
+
+      //we have to loop the entire demo array to look for the respective index
+      //and change its color
+      const demo = store.demo.map((elm, i) => {
+        if (i === index) elm.background = color;
+        return elm;
+      });
+
+      //reset the global store
+      setStore({ demo: demo });
+    },
+  };
 };
-
 export default getState;
