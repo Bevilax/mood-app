47a271999b191e918b4ce072f38613ecb4e1f0ae
Bevilax <matheusbevilacqua@hotmail.com>
2022-06-09 00:00:01 +0000

    addingHshdPWD

---
+++ src/api/hashed_pwd_user.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.hybrid import hybrid_property
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    _password = db.Column(db.String(120), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = generate_password_hash(password)
+
+    def check_password_hash(self, password):
+        return check_password_hash(self.password, password)
--- src/api/routes.py
+++ src/api/routes.py
@@ -3,6 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
+from api.hashed_pwd_user import db, User
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -29,3 +30,14 @@ def protected():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("pass", None)
+    user = User.query.filter_by(email=email).one_or_none()
+    if user is not None:
+        if user.check_password_hash(password):
+            access_token = create_access_token(identity=email)
+            return jsonify(access_token=access_token)
+    return jsonify({"msg": "Invalid cedentials."}), 401
