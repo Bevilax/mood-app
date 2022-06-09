3da3d29496a9ae961a44cb120b25f31ed67c0166
Bevilax <matheusbevilacqua@hotmail.com>
2022-06-09 23:04:52 +0000

    registrationAPI

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,6 +3,17 @@ import os
 from flask_admin import Admin
 from .models import db, User
 from flask_admin.contrib.sqla import ModelView
+from wtforms.fields import PasswordField
+
+class UserView(ModelView):
+    column_list = ['email', 'is_active']
+    column_editable_list = ['is_active',]
+    column_exclude_list = ['_password', ]
+    create_modal = True
+    edit_modal = True
+    form_extra_fields = {
+        'password': PasswordField('password')
+    }
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
@@ -11,7 +22,7 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(UserView(User, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/routes.py
+++ src/api/routes.py
@@ -3,7 +3,6 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
-from api.hashed_pwd_user import db, User
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -13,17 +12,6 @@ import os
 
 api = Blueprint('api', __name__)
 
-
-@api.route("/token", methods=["POST"])
-def create_token():
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
-    if email != "test" or password != "test":
-        return jsonify({"msg": "Bad username or password"}), 401
-
-    access_token = create_access_token(identity=email)
-    return jsonify(access_token=access_token)
-
 @api.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
@@ -31,7 +19,7 @@ def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
 
-@api.route("/login", methods=["POST"])
+@api.route("/token", methods=["POST"])
 def login():
     email = request.json.get("email", None)
     password = request.json.get("pass", None)
@@ -41,3 +29,16 @@ def login():
             access_token = create_access_token(identity=email)
             return jsonify(access_token=access_token)
     return jsonify({"msg": "Invalid cedentials."}), 401
+
+@api.route("/signup", methods=["POST"])
+def signup ():
+    email = request.json.get("email", None)
+    password = request.json.get("pass", None)
+    user = User.query.filter_by(email=email).one_or_none()
+    if user is None:
+        user = User(email=email, password=password, is_active=True)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"msg": "You created your account"}), 201
+    else:
+        return jsonify({"msg": "Your account already exists, please sign in"}), 406
