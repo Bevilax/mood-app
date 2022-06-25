f89615d38f24bfc96bb910e875740abf2b20225a
bryanhayslip <bryan.hayslip@gmail.com>
2022-06-25 00:47:50 +0000

    added map on EventsPage, not functioning yet

--- src/front/js/pages/eventspage.js
+++ src/front/js/pages/eventspage.js
@@ -1,18 +1,34 @@
 import React, { useState, useEffect, useContext } from "react";
 import "../../styles/emotions.css";
 import { Context } from "../store/appContext";
+import { EventCard } from "../component/eventcard";
 
 export const EventsPage = () => {
   const { store, actions } = useContext(Context);
+  const [results, setResults] = React.useState([]);
+  const ourFunction = actions.getEventByZipAndTaxonomy();
+
+  //   React.useEffect(() => {
+  //     actions.getEventByZipAndTaxonomy();
+  //   }, []);
 
   React.useEffect(() => {
-    actions.getEventByZipAndTaxonomy();
+    const fn = async () => {
+      const apiResults = await ourFunction;
+      setResults(apiResults);
+    };
+    fn();
   }, []);
   return (
     <div>
       <h3>Event results below:</h3>
       <br></br>
-      {JSON.stringify(store.events)}
+      <div className="d-flex flex-row flex-nowrap overflow-auto">
+        {results.map((item, index) => (
+          <EventCard event={item} uid={index + 1} key={index} />
+        ))}
+      </div>
+      {/* {JSON.stringify(store.events)} */}
     </div>
   );
 };
