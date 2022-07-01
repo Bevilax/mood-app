147190d7881ed333e298245989410ca1da25768c
bryanhayslip <bryan.hayslip@gmail.com>
2022-07-01 17:30:27 +0000

    working on map function

--- src/front/js/component/eventcard.js
+++ src/front/js/component/eventcard.js
@@ -2,8 +2,24 @@ import React from "react";
 
 export const EventCard = (props) => {
   return (
-    <div className="card">
-      <h5 className="card-title">props go here</h5>
+    <div className="border border-danger col-3 m-3">
+      <div className="card card-block">
+        <h5 className="card-title">{props.events.performer.name}</h5>
+      </div>
     </div>
+    // <div className="border border-danger col-3 m-3">
+    //   <div className="card card-block">
+    //     <h5 className="card-title">{props.event.name}</h5>
+    //     {/* props.event.performers.name */}
+    //     {/* <img src={Starwars} /> */}
+    //     <div className="card-footer d-flex">
+    //       {/* <div className="left-button">
+    //     <a href={"/people/" + props.uid} className="btn btn-primary">
+    //       {props.character.name}
+    //     </a>
+    //     </div> */}
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
--- src/front/js/pages/eventspage.js
+++ src/front/js/pages/eventspage.js
@@ -6,29 +6,23 @@ import { EventCard } from "../component/eventcard";
 export const EventsPage = () => {
   const { store, actions } = useContext(Context);
   const [results, setResults] = React.useState([]);
-  const ourFunction = actions.getEventByZipAndTaxonomy();
+  const event = store.events;
 
-  // React.useEffect(() => {
-  //   actions.getEventByZipAndTaxonomy(95125, 1000000);
-  // }, []);
+  React.useEffect(() => {
+    const fn = async () => {
+      const apiEvents = await actions.getEventByZipAndTaxonomy();
+      setResults(apiEvents);
+    };
+    fn();
+  }, []);
 
-  // React.useEffect(() => {
-  //   const fn = async () => {
-  //     const apiResults = await ourFunction;
-  //     setResults(apiResults);
-  //   };
-  //   fn();
-  // }, []);
   return (
-    <div>
-      {/* <h3>Event results below:</h3>
-      <br></br>
-      <div className="d-flex flex-row flex-nowrap overflow-auto">
-        {results.map((item, index) => (
-          <EventCard event={item} uid={index + 1} key={index} />
-        ))}
-      </div> */}
-      {JSON.stringify(store.events)}
+    <div className="d-flex flex-row flex-nowrap overflow-auto">
+      {results.map((item, index) => (
+        <EventCard event={item} key={index} />
+      ))}
     </div>
+
+    //   {JSON.stringify(store.events)}
   );
 };
