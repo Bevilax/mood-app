baceed2f281bab273e3c7384d9ac629e781e067d
bryanhayslip <bryan.hayslip@gmail.com>
2022-07-01 20:42:06 +0000

    more small updates to map function, in progress

--- src/front/js/component/eventcard.js
+++ src/front/js/component/eventcard.js
@@ -4,7 +4,7 @@ export const EventCard = (props) => {
   return (
     <div className="border border-danger col-3 m-3">
       <div className="card card-block m-8">
-        <h5 className="card-title">{props.title}</h5>
+        <h5 className="card-title">{props.results.title}</h5>
       </div>
     </div>
   );
