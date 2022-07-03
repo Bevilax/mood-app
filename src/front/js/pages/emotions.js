11a6af1db05aa8cece8d309ae80c3033601729da
bryanhayslip <bryan.hayslip@gmail.com>
2022-07-03 02:37:52 +0000

    added onClick events to each emotion button

--- src/front/js/pages/emotions.js
+++ src/front/js/pages/emotions.js
@@ -18,7 +18,11 @@ export const Emotions = () => {
         id="btncheck1"
         autocomplete="off"
         onClick={(e) => {
-          actions.getEventByZipAndTaxonomy(3000000);
+          actions.getEventByZipAndTaxonomy(1060000);
+          actions.getEventByZipAndTaxonomy(1110000);
+          actions.getEventByZipAndTaxonomy(1120000);
+          actions.getEventByZipAndTaxonomy(1150000);
+          actions.getEventByZipAndTaxonomy(2010000);
         }}
       />
 
@@ -32,8 +36,14 @@ export const Emotions = () => {
         id="btncheck2"
         autocomplete="off"
         onClick={(e) => {
-          actions.getEventByZipAndTaxonomy(1000000);
-          actions.getEventByZipAndTaxonomy(2000000);
+          actions.getEventByZipAndTaxonomy(1020000);
+          actions.getEventByZipAndTaxonomy(1030000);
+          actions.getEventByZipAndTaxonomy(1040000);
+          actions.getEventByZipAndTaxonomy(1080000);
+          actions.getEventByZipAndTaxonomy(1140000);
+          actions.getEventByZipAndTaxonomy(2010000);
+          actions.getEventByZipAndTaxonomy(3050000);
+          actions.getEventByZipAndTaxonomy(3090000);
         }}
       />
 
@@ -46,6 +56,13 @@ export const Emotions = () => {
         className="btn-check"
         id="btncheck3"
         autocomplete="off"
+        onClick={(e) => {
+          actions.getEventByZipAndTaxonomy(1100000);
+          actions.getEventByZipAndTaxonomy(1100000);
+          actions.getEventByZipAndTaxonomy(1130000);
+          actions.getEventByZipAndTaxonomy(1150000);
+          actions.getEventByZipAndTaxonomy(3020000);
+        }}
       />
       <label className="btn btn-outline fadingout" for="btncheck3">
         Artistic
@@ -55,6 +72,11 @@ export const Emotions = () => {
         className="btn-check"
         id="btncheck4"
         autocomplete="off"
+        onClick={(e) => {
+          actions.getEventByZipAndTaxonomy(1090000);
+          actions.getEventByZipAndTaxonomy(3010000);
+          actions.getEventByZipAndTaxonomy(3060000);
+        }}
       />
       <label className="btn btn-outline fadingout" for="btncheck4">
         Introspective
@@ -64,6 +86,12 @@ export const Emotions = () => {
         className="btn-check"
         id="btncheck5"
         autocomplete="off"
+        onClick={(e) => {
+          actions.getEventByZipAndTaxonomy(1010000);
+          actions.getEventByZipAndTaxonomy(1120000);
+          actions.getEventByZipAndTaxonomy(3030000);
+          actions.getEventByZipAndTaxonomy(3090000);
+        }}
       />
       <label className="btn btn-outline fadingout" for="btncheck5">
         Social
@@ -73,6 +101,13 @@ export const Emotions = () => {
         className="btn-check"
         id="btncheck6"
         autocomplete="off"
+        onClick={(e) => {
+          actions.getEventByZipAndTaxonomy(1010000);
+          actions.getEventByZipAndTaxonomy(1050000);
+          actions.getEventByZipAndTaxonomy(1100000);
+          actions.getEventByZipAndTaxonomy(1140000);
+          actions.getEventByZipAndTaxonomy(3070000);
+        }}
       />
       <label className="btn btn-outline fadingout" for="btncheck6">
         Lonely
@@ -82,6 +117,11 @@ export const Emotions = () => {
         className="btn-check"
         id="btncheck7"
         autocomplete="off"
+        onClick={(e) => {
+          actions.getEventByZipAndTaxonomy(1060000);
+          actions.getEventByZipAndTaxonomy(1130000);
+          actions.getEventByZipAndTaxonomy(3070000);
+        }}
       />
       <label className="btn btn-outline fadingout" for="btncheck7">
         Sad
@@ -91,9 +131,14 @@ export const Emotions = () => {
         className="btn-check"
         id="btncheck8"
         autocomplete="off"
+        onClick={(e) => {
+          actions.getEventByZipAndTaxonomy(1140000);
+          actions.getEventByZipAndTaxonomy(3010000);
+          actions.getEventByZipAndTaxonomy(3080000);
+        }}
       />
       <label className="btn btn-outline fadingout" for="btncheck8">
-        Something
+        Stressed
       </label>
       <input
         type="checkbox"
