8f4ccda550fa9c7affa641c561fe7850521b282d
Bevilax <matheusbevilacqua@hotmail.com>
2022-07-06 01:12:45 +0000

    changed the style of the cards but not done yet

--- src/front/js/component/eventcard.js
+++ src/front/js/component/eventcard.js
@@ -3,17 +3,24 @@ import "../../styles/eventcard.css";
 
 export const EventCard = (props) => {
   return (
-    <div className="border border-danger">
-      <div className="card">
-        <p>{props.title}</p>
-        <p>{props.type}</p>
-        <p>{props.eventtime}</p>
-        <p>Location: {props.venuename}</p>
-        <p>{props.perName}</p>
-
-        <img src={props.image} />
+    <div className="col-3 m-3">
+      <div className="card-deck">
+        <div className="card">
+          <img className="card-img-top" src={props.image} />
+          <div className="card-body">
+            <h5 className="card-title">
+              <p>{props.title}</p>
+            </h5>
+            <div className="card-text">
+              <p>{props.eventtime}</p>
+              <p>Location: {props.venuename}</p>
+            </div>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
     </div>
-    // <div className="border border-danger col-3 m-3"></div>
   );
 };
