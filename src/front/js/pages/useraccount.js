1b4b7784e4bf4699f282189e09910611dde3fe02
bryanhayslip <bryan.hayslip@gmail.com>
2022-07-06 00:34:49 +0000

    created UserAccount component, updated routes.py

---
+++ src/front/js/pages/useraccount.js
@@ -0,0 +1,32 @@
+import React, { useContext, useState } from "react";
+import "../../styles/home.css";
+import { useHistory } from "react-router-dom";
+import { Context } from "../store/appContext";
+import "../../styles/useraccount.css";
+
+export const UserAccount = () => {
+  return [
+    <div className="whole">
+      <div className="text-center mt-5 Form">
+        <div className="accountinfo">
+          <h1 className="accountheader">My Account</h1>
+          <div>
+            {/* <h5>Email: {User.email}</h5> */}
+
+            <br />
+            <br />
+            {/* <input
+                type="text"
+                placeholder="zipcode"
+                value={zipcode}
+                onChange={(e) => setZipcode(e.target.value)}
+              ></input> */}
+            <br />
+            <br />
+            <button className="changezipcode">Change my Zip Code</button>
+          </div>
+        </div>
+      </div>
+    </div>,
+  ];
+};
