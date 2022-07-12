2a5693c688e3145a640625559df81eb57c4e1f87
bryanhayslip <bryan.hayslip@gmail.com>
2022-07-12 02:41:24 +0000

    updated moodydescription/removed 3rd line

--- src/front/js/component/moodydescription.js
+++ src/front/js/component/moodydescription.js
@@ -8,9 +8,6 @@ export const MoodyDescription = () => {
       <div className="desc col-4 p-3">
         <h5>
           Select a mood to start <br></br>You can choose as many as you like
-          <br></br>
-          If you want to see more events, just click on the Moody logo on top of
-          the page to come back and select another mood
         </h5>
       </div>
       <div className="col-4"></div>
